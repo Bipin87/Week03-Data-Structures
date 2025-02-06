926749b07b6cf7923a5856a9261246182d3e7740
Bipin87 <sahubipin484@gmail.com>
2025-02-06 10:20:09 +0530

    [Bipin Kumar Sahu] Add. search first and last occurrence element in array code

---
+++ Day-05/src/main/java/com/capgemini/binary_search/find_first_last_occurrence_element_array/FirstAndLastOccurrenceElement.java
@@ -0,0 +1,35 @@
+package com.capgemini.binary_search.find_first_last_occurrence_element_array;
+
+public class FirstAndLastOccurrenceElement {
+    public static int[] findFirstAndLastOccurrence(int[] arr,int target){
+        int[] ans = {-1,-1};
+        int left = 0,right = arr.length - 1, mid = 0;
+        // Loop until left is smaller than right or the elements is not found and found the first occurrence
+        while(left <= right){
+            mid = left + (right - left) / 2;
+            // Checks if mid is equal to target or not
+            if (arr[mid] == target){
+                ans[0] = mid;   // Update the first occurrence and move the pointer left
+                right = mid - 1;
+            }
+            else if(arr[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+        left = 0;
+        right = arr.length - 1;
+        while(left <= right){
+            mid = left + (right - left) / 2;
+            // Checks if mid is equal to target or not
+            if (arr[mid] == target){
+                ans[1] = mid;   // Update the last occurrence and move the pointer right
+                left = mid + 1;
+            }
+            else if(arr[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+
+
+
+        return ans;
+    }
+}
