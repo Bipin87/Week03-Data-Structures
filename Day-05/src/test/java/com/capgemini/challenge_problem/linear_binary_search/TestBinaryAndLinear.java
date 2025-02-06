55345dcec4e9f88911e149b201e1857b879c0500
Bipin87 <sahubipin484@gmail.com>
2025-02-06 10:46:24 +0530

    [Bipin Kumar Sahu] Add.test first missing positive number and target index code

--- Day-05/src/test/java/com/capgemini/challenge_problem/linear_binary_search/TestBinaryAndLinear.java
+++ Day-05/src/test/java/com/capgemini/challenge_problem/linear_binary_search/TestBinaryAndLinear.java
@@ -1,14 +1,14 @@
-package com.capgemini.challenge_problem;
+package com.capgemini.challenge_problem.linear_binary_search;
 
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import static com.capgemini.challenge_problem.linear_search_first_missing_positive_integer.FirstMissPosInteger.searchFirstMissingPositive;
+import static com.capgemini.challenge_problem.linear_and_binary_search.LinearAndBinarySearch.searchFirstMissingPositive;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestFirstMissing {
+public class TestBinaryAndLinear {
     @Test
     void linearSearchTest(){
         assertEquals(2,searchFirstMissingPositive(new ArrayList<>(Arrays.asList(1,3,4,5,6))));
